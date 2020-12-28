9854d9998a03bfb185210c3f744ae09b86d99a73
@hugolgc <53827675+hugolgc@users.noreply.github.com>
2020-12-28 11:22:12 +0100

    Update default.php

--- views/templates/default.php
+++ views/templates/default.php
@@ -2,6 +2,7 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Genesis</title>
     <link rel="shortcut icon" href="<?= src('favicon.ico') ?>">
     <link rel="stylesheet" href="<?= css('genesis') ?>">
