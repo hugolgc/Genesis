ae64036c9e2cb2ec82bd4f5fb5f0c3d2d4cf05b3
@hugolgc <53827675+hugolgc@users.noreply.github.com>
2021-02-09 11:20:57 +0100

    Update autoload.php

--- genesis/autoload.php
+++ genesis/autoload.php
@@ -21,5 +21,6 @@ function stopSession(): void
   } session_destroy();
 }
 
+function trash($name, $folder = 'figures', $path = '../public/assets/') { unlink($path . $folder . $name); }
 function secure(string $slug): string { return trim(stripslashes(htmlspecialchars($slug))); }
 function location(string $path = '/'): void { header("location: $path"); }
